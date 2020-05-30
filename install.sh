8237a29d99809f7bf2e51ca0b9e3463b5df27f34
FlUxIUS <sebastien.dudek@penthertz.com>
2020-05-30 18:29:56 +0200

    Starting a small install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+git submodule update --init --recursive
+pip install -r requirements.txt
+rm -R bin
+mkdir bin
+wget https://github.com/FlUxIuS/V2Gdecoder/releases/download/v1/V2Gdecoder.jar --directory-prefix=bin
+rm V2GDecoder_default_run.sh
+tee -a V2GDecoder_default_run.sh << END
+#!/usr/bin/bash
+
+java -jar bin/V2Gdecoder.jar -w
+END
+chmod +x V2GDecoder_default_run.sh
