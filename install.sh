c29f56a6bc1a4adff482efa4316c5a242f0f1a4b
Sébastien Dudek <fluxius@gmail.com>
2024-06-28 10:37:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git submodule update --init --recursive
 pip install -r requirements.txt
 rm -R bin
 mkdir bin
-wget https://github.com/FlUxIuS/V2Gdecoder/releases/download/v1/V2Gdecoder.jar --directory-prefix=bin
+wget https://github.com/FlUxIuS/V2Gdecoder/releases/download/v1.2/V2Gdecoder.jar --directory-prefix=bin
 rm V2GDecoder_default_run.sh
 tee -a V2GDecoder_default_run.sh << END
 #!/usr/bin/bash
